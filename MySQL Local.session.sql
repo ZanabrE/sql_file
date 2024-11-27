23ef20934142e8bf0aa0fccd4c44a6ea91712532
Ernesto L Zanabria <ernesto.zanabria1979@gmail.com>
2024-11-26 20:57:03 -0500

    modified: MySQL Local.session.sql

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -1,19 +1,10 @@
 CREATE TABLE IF NOT EXISTS USERS (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    USERNAME VARCHAR(255) NOT NULL,
-    EMAIL VARCHAR(255) NOT NULL
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
-INSERT INTO USERS (
-    USERNAME,
-    EMAIL
-) VALUES (
-    'john_doe',
-    'john@example.com'
-) (
-    'jane_smith',
-    'jane@example.com'
-) (
-    'bob_jones',
-    'bob@example.com'
-)
+INSERT INTO users (username, email) VALUES 
+('john_doe', 'john@example.com'), 
+('jane_smith', 'jane@example.com'), 
+('bob_jones', 'bob@example.com');
